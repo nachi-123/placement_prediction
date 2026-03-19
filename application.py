f04e3f0da7d63ad14f444949936f165883f72e71
nachi-123 <nachikethus2005@gmail.com>
2026-03-19 18:49:11 +0530

    Improve code readability by adding blank lines
    JUST TESTING

--- application.py
+++ application.py
@@ -36,7 +36,8 @@ def predict_datapoint():
         predict_pipeline=PredictPipeline()
         results=predict_pipeline.predict(pred_df)
         return render_template('home.html',results=results[0])
-    
+
+
 
 if __name__=="__main__":
     url = "http://127.0.0.1:5000/predictdata"
